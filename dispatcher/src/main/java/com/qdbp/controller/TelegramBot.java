7b70a711ab91489f83e529e5f1e2e865e815bb19
RoninWay1618 <serzh.loshilov@gmail.com>
2025-04-14 20:45:20 +0300

    Создал контроллер для распределения сообщений по очередям в брокере сообщений и создал сервисы, с помощью которых буду интегрироваться с  RabbitMQ. Также были исправлены несколько обнаруженных багов.

--- dispatcher/src/main/java/com/qdbp/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/qdbp/controller/TelegramBot.java
@@ -1,13 +1,14 @@
 package com.qdbp.controller;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
 
 @Component
 @Log4j
@@ -17,6 +18,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+    private UpdateController updateController;
+
+    public TelegramBot(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @PostConstruct
+    public void init(){
+        updateController.registerBot(this);
+    }
 
     @Override
     public String getBotUsername() {
@@ -30,13 +41,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var originalMessage = update.getMessage();
-        log.debug(originalMessage.getText());
-
-        var response = new SendMessage();
-        response.setChatId(originalMessage.getChatId().toString());
-        response.setText("Hello from IDEA");
-        sendAnswerMessage(response);
+        updateController.processUpdate(update);
     }
 
     public void sendAnswerMessage(SendMessage message) {
