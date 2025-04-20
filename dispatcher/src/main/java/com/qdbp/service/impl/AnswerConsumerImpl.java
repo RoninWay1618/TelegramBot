4b027277504d999220bad1ba9c78078d18e4ecf7
RoninWay1618 <serzh.loshilov@gmail.com>
2025-04-20 21:42:31 +0300

    Создал микросервис node, интегрировал его с RabbitMQ и настроил считывание сообщений из брокера сообщения с помощью аннотации @RabbitListener().

---
+++ dispatcher/src/main/java/com/qdbp/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,25 @@
+package com.qdbp.service.impl;
+
+import com.qdbp.controller.UpdateController;
+import com.qdbp.service.AnswerConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static com.qdbp.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
