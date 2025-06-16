8d77133a48ec76ed52e188ab708dee256ae3119f
RoninWay1618 <serzh.loshilov@gmail.com>
2025-06-16 21:56:47 +0300

    Улучшил код по работе с файлами и заменил технологию long polling на webhook для интеграции с телеграммом.

--- dispatcher/src/main/java/com/qdbp/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/com/qdbp/service/impl/AnswerConsumerImpl.java
@@ -1,6 +1,6 @@
 package com.qdbp.service.impl;
 
-import com.qdbp.controller.UpdateController;
+import com.qdbp.controller.UpdateProcessor;
 import com.qdbp.service.AnswerConsumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -11,15 +11,15 @@ import static com.qdbp.model.RabbitQueue.ANSWER_MESSAGE;
 @Service
 public class AnswerConsumerImpl implements AnswerConsumer {
 
-    private final UpdateController updateController;
+    private final UpdateProcessor updateProcessor;
 
-    public AnswerConsumerImpl(UpdateController updateController) {
-        this.updateController = updateController;
+    public AnswerConsumerImpl(UpdateProcessor updateProcessor) {
+        this.updateProcessor = updateProcessor;
     }
 
     @Override
     @RabbitListener(queues = ANSWER_MESSAGE)
     public void consume(SendMessage sendMessage) {
-        updateController.setView(sendMessage);
+        updateProcessor.setView(sendMessage);
     }
 }
